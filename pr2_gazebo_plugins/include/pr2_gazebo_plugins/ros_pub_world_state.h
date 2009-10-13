6402127ed4fec5b23c090bafa2c47fe012ea6c71
hsu <hsu@7275ad9f-c29b-430a-bdc5-66f4b3af1622>
2009-10-13 00:25:47 +0000

    test plugins for broadcasting world state

---
+++ pr2_gazebo_plugins/include/pr2_gazebo_plugins/ros_pub_world_state.h
@@ -0,0 +1,162 @@
+/*
+ *  Gazebo - Outdoor Multi-Robot Simulator
+ *  Copyright (C) 2003  
+ *     Nate Koenig & Andrew Howard
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+/*
+ * Desc: Actuator array controller for a Pr2 robot.
+ * Author: Nathan Koenig
+ * Date: 19 Sept 2007
+ * SVN: $Id$
+ */
+#ifndef ROS_TIME_HH
+#define ROS_TIME_HH
+
+#include <gazebo/Controller.hh>
+#include <gazebo/Body.hh>
+#include <gazebo/Model.hh>
+#include <gazebo/World.hh>
+#include <gazebo/Simulator.hh>
+#include <pr2_gazebo_plugins/WorldState.h>
+
+#include <ros/ros.h>
+
+#include <map>
+#include <vector>
+#include <string>
+
+namespace gazebo
+{
+/// @addtogroup gazebo_dynamic_plugins Gazebo Dynamic Plugins
+/// @{
+/** \defgroup RosPubWorldState
+
+  \brief A sample gazebo dynamic plugin
+  
+  This is a gazebo controller that does nothing
+
+  Example Usage:
+  \verbatim
+    <model:physical name="robot_model1">
+
+      <controller:ros_pub_world_state name="ros_pub_world_state_controller" plugin="libros_pub_world_state.so">
+        <alwaysOn>true</alwaysOn>
+        <updateRate>1000.0</updateRate>
+      </controller:ros_pub_world_state>
+
+      <xyz>0.0 0.0 0.02</xyz>
+      <rpy>0.0 0.0 0.0 </rpy>
+
+      <!-- a box -->
+      <body:box name="test_block">
+          <massMatrix>true</massMatrix>
+          <mass>1000</mass>
+          <ixx>100</ixx>
+          <ixy>0</ixy>
+          <ixz>0</ixz>
+          <iyy>100</iyy>
+          <iyz>0</iyz>
+          <izz>100</izz>
+          <cx>0</cx>
+          <cy>0</cy>
+          <cz>0</cz>
+          <xyz>0 0 0.002</xyz>
+          <rpy>0 -0 0</rpy>
+          <geom:box name="test_block_collision_geom">
+              <xyz>0 0 10</xyz>
+              <rpy>0 0 0</rpy>
+              <size>20 20 20</size>
+              <visual>
+                  <xyz>0 0 0</xyz>
+                  <rpy>0 -0 0</rpy>
+                  <scale>20 20 20</scale>
+                  <mesh>unit_box</mesh>
+                  <material>Gazebo/GrassFloor</material>
+              </visual>
+          </geom:box>
+      </body:box>
+
+    </model:physical>
+  \endverbatim
+ 
+\{
+*/
+
+class RosPubWorldState : public Controller
+{
+  /// \brief Constructor
+  /// \param parent The parent entity, must be a Model or a Sensor
+  public: RosPubWorldState(Entity *parent);
+
+  /// \brief Destructor
+  public: virtual ~RosPubWorldState();
+
+  /// \brief Load the controller
+  /// \param node XML config node
+  protected: virtual void LoadChild(XMLConfigNode *node);
+
+  /// \brief Init the controller
+  protected: virtual void InitChild();
+
+  /// \brief Update the controller
+  protected: virtual void UpdateChild();
+
+  /// \brief Finalize the controller
+  protected: virtual void FiniChild();
+
+  /// \brief: keep track of number of connections
+  private: int worldStateConnectCount;
+  private: void WorldStateConnect();
+  private: void WorldStateDisconnect();
+
+  /// \brief: Message for sending world state
+  private: pr2_gazebo_plugins::WorldState worldStateMsg;
+
+  /// \bridf: Keep list of all models in the world
+  private: const std::map<std::string,gazebo::Body*> *bodies;
+
+  /// \bridf: parent should be a model
+  private: gazebo::Model* parent_model_;
+
+  /// \bridf: ros node handle and publisher
+  private: ros::NodeHandle* rosnode_;
+  private: ros::Publisher pub_;
+
+  /// \brief A mutex to lock access to fields that are used in message callbacks
+  private: boost::mutex lock;
+
+  /// \brief for setting ROS name space
+  private: ParamT<std::string> *robotNamespaceP;
+  private: std::string robotNamespace;
+
+  /// \brief topic name
+  private: ParamT<std::string> *topicNameP;
+  private: std::string topicName;
+
+  /// \brief frame transform name, should match link name
+  private: ParamT<std::string> *frameNameP;
+  private: std::string frameName;
+
+};
+
+/** \} */
+/// @}
+
+}
+#endif
+
---
+++ pr2_gazebo_plugins/src/ros_pub_world_state.cpp
@@ -0,0 +1,222 @@
+/*
+ *  Gazebo - Outdoor Multi-Robot Simulator
+ *  Copyright (C) 2003
+ *     Nate Koenig & Andrew Howard
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+/*
+ * Desc: Empty gazebo plugin
+ * Author: John Hsu
+ * Date: 24 July 2009
+ * SVN info: $Id$
+ */
+
+
+#include <algorithm>
+#include <assert.h>
+
+#include <pr2_gazebo_plugins/ros_pub_world_state.h>
+
+#include <gazebo/Global.hh>
+#include <gazebo/XMLConfig.hh>
+#include <gazebo/gazebo.h>
+#include <gazebo/GazeboError.hh>
+#include <gazebo/ControllerFactory.hh>
+
+using namespace gazebo;
+
+GZ_REGISTER_DYNAMIC_CONTROLLER("ros_pub_world_state", RosPubWorldState);
+
+////////////////////////////////////////////////////////////////////////////////
+// Constructor
+RosPubWorldState::RosPubWorldState(Entity *parent)
+    : Controller(parent)
+{
+  this->parent_model_ = dynamic_cast<Model*>(this->parent);
+
+  if (!this->parent_model_)
+    gzthrow("GazeboMechanismControl controller requires a Model as its parent");
+
+  Param::Begin(&this->parameters);
+  this->robotNamespaceP = new ParamT<std::string>("robotNamespace", "/", 0);
+  this->topicNameP = new ParamT<std::string>("topicName", "gazebo_world_state", 0);
+  this->frameNameP = new ParamT<std::string>("frameName", "base_link", 0);
+  Param::End();
+
+  this->worldStateConnectCount = 0;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Destructor
+RosPubWorldState::~RosPubWorldState()
+{
+  delete this->robotNamespaceP;
+  delete this->topicNameP;
+  delete this->frameNameP;
+  delete this->rosnode_;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Load the controller
+void RosPubWorldState::LoadChild(XMLConfigNode *node)
+{
+  this->robotNamespaceP->Load(node);
+  this->robotNamespace = this->robotNamespaceP->GetValue();
+
+  int argc = 0;
+  char** argv = NULL;
+  ros::init(argc,argv,"gazebo");
+  this->rosnode_ = new ros::NodeHandle(this->robotNamespace);
+
+  this->topicNameP->Load(node);
+  this->topicName = this->topicNameP->GetValue();
+  this->frameNameP->Load(node);
+  this->frameName = this->frameNameP->GetValue();
+
+  this->pub_ = this->rosnode_->advertise<pr2_gazebo_plugins::WorldState>(this->topicName,1,
+    boost::bind( &RosPubWorldState::WorldStateConnect, this),
+    boost::bind( &RosPubWorldState::WorldStateDisconnect, this));
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Someone subscribes to me
+void RosPubWorldState::WorldStateConnect()
+{
+  this->worldStateConnectCount++;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Someone subscribes to me
+void RosPubWorldState::WorldStateDisconnect()
+{
+  this->worldStateConnectCount--;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Initialize the controller
+void RosPubWorldState::InitChild()
+{
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Update the controller
+void RosPubWorldState::UpdateChild()
+{
+  /***************************************************************/
+  /*                                                             */
+  /*  this is called at every update simulation step             */
+  /*                                                             */
+  /***************************************************************/
+
+  /***************************************************************/
+  /*                                                             */
+  /*  publish                                                    */
+  /*                                                             */
+  /***************************************************************/
+  double cur_time = Simulator::Instance()->GetSimTime();
+
+  this->bodies = this->parent_model_->GetBodies();
+  if (this->bodies)
+  {
+    this->lock.lock();
+
+    // Add Frame Name
+    this->worldStateMsg.header.frame_id = this->frameName;
+    this->worldStateMsg.header.stamp.fromSec(cur_time);
+
+    this->worldStateMsg.set_name_size(this->bodies->size());
+    this->worldStateMsg.set_pose_size(this->bodies->size());
+    this->worldStateMsg.set_twist_size(this->bodies->size());
+    this->worldStateMsg.set_wrench_size(this->bodies->size());
+
+    // Iterate through all bodies
+    std::map<std::string, Body*>::const_iterator biter;
+    int count = 0;
+    for (biter=this->bodies->begin(); biter!=this->bodies->end(); biter++)
+    {
+      //ROS_ERROR("body name: %s",(biter->second)->GetName().c_str());
+      // get name
+      this->worldStateMsg.name[count] =  biter->second->GetName();
+
+      // set pose
+      // get pose from simulator
+      Pose3d pose;
+      Quatern rot;
+      Vector3 pos;
+      // Get Pose/Orientation ///@todo: verify correctness
+      pose = (biter->second)->GetPose();
+
+      // apply xyz offsets and get position and rotation components
+      pos = pose.pos; // (add if there's offset) + this->xyzOffsets;
+      rot = pose.rot;
+      // apply rpy offsets
+      /* add if there's offsets
+      Quatern qOffsets;
+      qOffsets.SetFromEuler(this->rpyOffsets);
+      rot = qOffsets*rot;
+      rot.Normalize();
+      */
+    
+      this->worldStateMsg.pose[count].position.x    = pos.x;
+      this->worldStateMsg.pose[count].position.y    = pos.y;
+      this->worldStateMsg.pose[count].position.z    = pos.z;
+      this->worldStateMsg.pose[count].orientation.x = rot.x;
+      this->worldStateMsg.pose[count].orientation.y = rot.y;
+      this->worldStateMsg.pose[count].orientation.z = rot.z;
+      this->worldStateMsg.pose[count].orientation.w = rot.u;
+
+      // set velocities
+      // get Rates
+      Vector3 vpos = (biter->second)->GetPositionRate(); // get velocity in gazebo frame
+      Quatern vrot = (biter->second)->GetRotationRate(); // get velocity in gazebo frame
+      Vector3 veul = (biter->second)->GetEulerRate(); // get velocity in gazebo frame
+
+      // pass linear rates
+      this->worldStateMsg.twist[count].linear.x        = vpos.x;
+      this->worldStateMsg.twist[count].linear.y        = vpos.y;
+      this->worldStateMsg.twist[count].linear.z        = vpos.z;
+      // pass euler angular rates
+      this->worldStateMsg.twist[count].angular.x    = veul.x;
+      this->worldStateMsg.twist[count].angular.y    = veul.y;
+      this->worldStateMsg.twist[count].angular.z    = veul.z;
+
+      // get forces
+      Vector3 force = (biter->second)->GetForce(); // get velocity in gazebo frame
+      Vector3 torque = (biter->second)->GetTorque(); // get velocity in gazebo frame
+      this->worldStateMsg.wrench[count].force.x = force.x;
+      this->worldStateMsg.wrench[count].force.x = force.y;
+      this->worldStateMsg.wrench[count].force.x = force.z;
+      this->worldStateMsg.wrench[count].torque.x = torque.x;
+      this->worldStateMsg.wrench[count].torque.x = torque.y;
+      this->worldStateMsg.wrench[count].torque.x = torque.z;
+
+      count++;
+    }
+
+    this->pub_.publish(this->worldStateMsg);
+    this->lock.unlock();
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Finalize the controller
+void RosPubWorldState::FiniChild()
+{
+}
+
+
+
