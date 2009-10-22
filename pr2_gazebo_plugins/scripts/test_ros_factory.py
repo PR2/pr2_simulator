7b7bbaffed7e2037b27866d583abb191d07bc470
hsu <hsu@7275ad9f-c29b-430a-bdc5-66f4b3af1622>
2009-10-22 01:49:38 +0000

    test script for ros_factory

---
+++ pr2_gazebo_plugins/scripts/test_ros_factory.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import roslib; roslib.load_manifest('pr2_gazebo_plugins')
+
+import sys
+import string
+import rospy
+from pr2_gazebo_plugins.msg import GazeboModel
+from pr2_gazebo_plugins.srv import SpawnModel
+from pr2_gazebo_plugins.srv import DeleteModel
+from geometry_msgs.msg import Pose, Point, Quaternion
+
+def spawn_model_client_from_urdf_param(model_name,param_name):
+    rospy.wait_for_service('spawn_model')
+    try:
+        spawn_model= rospy.ServiceProxy('spawn_model', SpawnModel)
+        resp1 = spawn_model(GazeboModel(model_name,param_name,GazeboModel.URDF_PARAM_NAME,"/",Pose(Point(0,1,0),Quaternion())))
+        return resp1.success
+    except rospy.ServiceException, e:
+        print "Service call failed: %s"%e
+
+def usage():
+    print '''Commands:
+    list                 - List active models
+    spawn <name>         - Spawns and starts the model named <name>
+    del   <name>         - Stops and deletes the model named <name>
+    pause <name>         - Stops the model named <name>
+    start <name>         - Starts the model named <name>
+    '''
+
+    sys.exit(exit_code)
+
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        model_name = sys.argv[1]
+        param_name = sys.argv[2]
+    else:
+        print usage()
+        sys.exit(1)
+    success = spawn_model_client_from_urdf_param(model_name,param_name)
+    print "test spawning success",success
