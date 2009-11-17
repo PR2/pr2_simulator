1875230061c2f93a3f8107b351716cd0aa16fde6
hsu <hsu@7275ad9f-c29b-430a-bdc5-66f4b3af1622>
2009-11-17 22:06:11 +0000

    update from spawn_gazebo_model to gazebo_model.

--- pr2_gazebo_plugins/src/gazebo_ros_controller_manager.cpp
+++ pr2_gazebo_plugins/src/gazebo_ros_controller_manager.cpp
@@ -104,7 +104,7 @@ void GazeboRosControllerManager::LoadChild(XMLConfigNode *node)
   int argc = 0;
   char** argv = NULL;
   ros::init(argc,argv,"gazebo",ros::init_options::NoSigintHandler|ros::init_options::AnonymousName);
-  this->rosnode_ = new ros::NodeHandle(this->robotNamespace);  // namespace comes from spawn_gazebo_model
+  this->rosnode_ = new ros::NodeHandle(this->robotNamespace);  // namespace comes from gazebo_model spawner
   ROS_INFO("starting gazebo_ros_controller_manager plugin in ns: %s",this->robotNamespace.c_str());
 
   this->cm_ = new pr2_controller_manager::ControllerManager(&hw_,*this->rosnode_);
